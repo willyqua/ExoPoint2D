ea90e45550af3ee967095ae7b2bd04a9251ffa6a
willy <willy.quasevi@gmail.com>
2021-10-15 14:45:46 +0200

    exo1Point2D finish

--- src/fr/Run.java
+++ src/fr/Run.java
@@ -1,7 +1,25 @@
 package fr;
 
+import fr.dessin.Point2D;
+
+import java.awt.*;
+
 public class Run {
     public static void main(String [] args){
-        System.out.println("wil");
+
+    Point2D x = new Point2D(4,34);
+    Point2D y = new Point2D(10,35);
+
+    x.display();
+    x.translater(7,14);
+    x.display();
+
+    y.display();
+    y.translater(7,10);
+    y.display();
+
+
+
+
     }
 }
--- src/fr/dessin/Point2D.java
+++ src/fr/dessin/Point2D.java
@@ -1,6 +1,51 @@
 package fr.dessin;
 
+import java.util.ArrayList;
+
 public class Point2D {
 
+    private int x;
+    private int y;
 
+    private Point2D() {
+    }
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public void translater(double dX, int dY) {
+
+        this.x += dX;
+        this.y += dY;
+    }
+
+    public void display() {
+
+        ArrayList<Integer> point = new ArrayList<>();
+        point.add(this.x);
+        point.add(this.y);
+        System.out.println(point);
+
+
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
